8c08e2dc061627fae94ef7040d9cb8eb9d83dbb5
Atul Kumar Singh <107607785+atul611cs@users.noreply.github.com>
2023-06-17 19:20:18 +0530

    Create 1528. Shuffle String.cpp

---
+++ 1528. Shuffle String.cpp
@@ -0,0 +1,14 @@
+1528. Shuffle String
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) 
+    {
+        int n= s.length();
+        string r=s;
+        for(int i=0;i<n;i++)
+        {
+            r[indices[i]]=s[i];
+        }
+        return r;
+    }
+};
