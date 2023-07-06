c039c729dcdaa7e7650e96436288340eddd45f37
Atul Kumar Singh <107607785+atul611cs@users.noreply.github.com>
2023-07-06 20:46:18 +0530

    Create 136. Single Number.cpp

---
+++ 136. Single Number.cpp
@@ -0,0 +1,13 @@
+136. Single Number
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        int single=0;
+        for(int num:nums)
+        {
+            single^=num;
+        }
+        return single;
+    }
+};
